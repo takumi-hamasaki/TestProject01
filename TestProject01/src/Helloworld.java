42e3801762a317e043abcd876d408e2a3b8e79f9
takumi-hamasaki <hamasakitakumi0830gmail.com>
2024-10-06 22:18:36 +0900

    20241006

---
+++ TestProject01/src/Helloworld.java
@@ -0,0 +1,13 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i = 0; i <= 5; i++) {
+			String b = i % 2 == 0 ? "☆":"★";
+				System.out.println(b);
+			
+		}
+	}
+
+}
